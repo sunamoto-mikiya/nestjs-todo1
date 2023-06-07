41d2d8950157e1ba69250e39a42ea19bb87782f8
sunamoto-mikiya <miki38gtr@gmail.com>
2023-06-07 17:57:11 +0900

    todoコントローラに2つのメソッドを作成

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+    @Get("list")
+    getList() {
+        return [
+            {
+                title: "牛乳を買いに行く",
+                due_on: "2022-12-24",
+                done:false
+            }
+        ]
+    }
+
+    @Get("view")
+    getView() {
+        return [
+            {
+                view:"これはviewです"
+            }
+        ]
+    }
+}

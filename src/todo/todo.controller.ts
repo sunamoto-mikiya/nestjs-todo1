399dcbabca36dff297421a0d6e52f647a2d24e3c
sunamoto-mikiya <miki38gtr@gmail.com>
2023-06-09 14:07:24 +0900

    controllerでpostされてくる際の処理追加

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { takeLast } from 'rxjs';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './create-task.dto';
 
 @Controller('todo')
 export class TodoController {
@@ -14,6 +16,16 @@ export class TodoController {
             ...result
         ]
     }
+
+    @Post("")
+    async add(@Body() task: CreateTaskDto) {
+        const result = await this.prisma.task.create({
+            data:task
+        })
+        return {
+            status:"OK"
+        }
+        }
 
     @Get("view")
     getView() {
